ec589d928c8e54860569bfd610a6f3eb20a7d516
Abul Hassan Sheikh <asheikh@tdotperformance.ca>
2019-07-04 18:23:52 -0400

    Rename Files

--- O2_P5_SupportCode_PR_M_CoreRefreshFun.R
+++
@@ -1,8 +0,0 @@
-
-refresh = function(){
-
-source("//192.168.2.32/Group/Data Team/Abul/1. Development/1. FunctionalTools/R/1. ProductionReady/B1. SupportingCode/2. Refresh/RefreshFileRef.R")
-
-message("JAAVS Updated.")
-message("Author: Abul Hassan Sheikh")
-}
---
+++ O2_P6_SupportCode_PR_M_CoreRefreshFun.R
@@ -0,0 +1,9 @@
+
+refresh = function(){
+
+source("\\192.168.2.32\Group\Data Team\Abul\1. Code\O2_P6_SupportCode\O2_P5_SupportCode_PR_M_RefreshFileRef.R")
+
+
+message("JAAVS Updated.")
+message("Author: Abul Hassan Sheikh")
+}
